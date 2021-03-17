d68b93d8b97a6a874ba41dd361e98aa8c525a337
davidzie <davidzi@post.bgu.ac.il>
2021-03-17 17:45:32 +0200

    First commit. Add Robot listener.

--- src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/HelloWorld.java
+++ src/main/java/il/ac/bgu/cs/bp/samplebpjsproject/HelloWorld.java
@@ -5,13 +5,16 @@ import il.ac.bgu.cs.bp.bpjs.execution.listeners.PrintBProgramRunnerListener;
 import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 import il.ac.bgu.cs.bp.bpjs.model.ResourceBProgram;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Simple class running a BPjs program that selects "hello world" events.
  * @author michael
  */
 public class HelloWorld {
     
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException, TimeoutException {
         // This will load the program file  <Project>/src/main/resources/HelloBPjsWorld.js
         final BProgram bprog = new ResourceBProgram("HelloBPjsWorld.js");
         
@@ -19,7 +22,8 @@ public class HelloWorld {
 
         // Print program events to the console
         rnr.addListener( new PrintBProgramRunnerListener() );
-        
+        rnr.addListener( new RobotBProgramRunnerListener() );
+
         // go!
         rnr.run();
     }
