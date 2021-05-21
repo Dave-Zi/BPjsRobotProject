3e68a9dec740df53388d1094aad0d85974e9b032
davidzie <davidzi@post.bgu.ac.il>
2021-05-21 03:14:08 +0300

    Support Nicknames for boards and ports

--- src/main/resources/HelloBPjsWorld.js
+++ src/main/resources/HelloBPjsWorld.js
@@ -13,7 +13,7 @@ var AlgorithmEventSet = bp.EventSet("", function (e) {
 var direction = 1;
 
 bp.registerBThread("Do Something with Data", function () {
-    bp.sync({request: bp.Event("Subscribe", {"GrovePi": ["D4"]})});
+    bp.sync({request: bp.Event("Subscribe", {"GrovePi": ["D2", "D4"]})});
 
     while (true) {
         var e = bp.sync({waitFor: dataEventSet});
@@ -101,17 +101,20 @@ bp.registerBThread("Initiation", function () {
             {
                 "EV3":
                     [{
-                        "Port": "rfcomm0"
+                        "Name": "EV3_1",
+                        "Port": "rfcomm0",
+                        "2": {"Name": "UV3"}
                     }]
                 ,
                 "GrovePi":
                     [{
-                        "A0": "",
+                        "Name": "GrovePi1",
+                        "A0": {"Name": "", "Device": ""},
                         "A1": "",
                         "A2": "",
-                        "D2": "Led",
+                        "D2": {"Name": "MyLed", "Device": "Led"},
                         "D3": "",
-                        "D4": "Ultrasonic",
+                        "D4": {"Name": "UV", "Device": "Ultrasonic"},
                         "D5": "",
                         "D6": "",
                         "D7": "",
