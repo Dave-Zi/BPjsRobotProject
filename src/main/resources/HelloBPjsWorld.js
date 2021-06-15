666d0012b7178c8b322dbc60c05e8122d794d713
davidzie <davidzi@post.bgu.ac.il>
2021-06-15 19:07:46 +0300

    new js

--- src/main/resources/HelloBPjsWorld.js
+++ src/main/resources/HelloBPjsWorld.js
@@ -6,6 +6,7 @@ var dataEventSet = bp.EventSet("", function (e) {
     return e.name.equals("GetSensorsData");
 });
 
+
 bp.registerBThread("Forward", function () {
     bp.sync({request: bp.Event("Subscribe", {"EV3": ["UV_Sensor"]})});
 
@@ -14,8 +15,8 @@ bp.registerBThread("Forward", function () {
         var e = bp.sync({waitFor: dataEventSet});
         var data = JSON.parse(e.data);
         var distance = data.EV3.My_Ev3.UV_Sensor;
-        if (distance > 20){
-            bp.sync({request: bp.Event("Rotate", {"EV3": {"B": 60, "C": 60, "speed": 15}})});
+        if (distance > 30){
+            bp.sync({request: bp.Event("Drive", {"EV3": {"B": 10, "C": 10}})});
         }
     }
 });
@@ -27,21 +28,21 @@ bp.registerBThread("Stop", function () {
         var e = bp.sync({waitFor: dataEventSet});
         var data = JSON.parse(e.data);
         var distance = data.EV3.My_Ev3.UV_Sensor;
-        if (distance < 20){
-            bp.sync({request: bp.Event("Rotate", {"EV3": {"B": 360, "C": 360, "speed": 15}})});
+        if (distance < 30){
+            bp.sync({request: bp.Event("Drive", {"EV3": {"B": 10, "C": -10}})});
         }
     }
 });
 
 bp.registerBThread("Manually turn around", function () {
-    bp.sync({request: bp.Event("Subscribe", {"EV3": ["UV_Sensor"]})});
+    bp.sync({request: bp.Event("Subscribe", {"GrovePi": ["D4"]})});
 
     while (true) {
         var e = bp.sync({waitFor: dataEventSet});
         var data = JSON.parse(e.data);
         var distance = data.GrovePi._1.D4;
         if (distance < 10){
-            bp.sync({request: bp.Event("Rotate", {"EV3": {"B": 360, "C": 360, "speed": 15}})});
+            bp.sync({request: bp.Event("Rotate", {"EV3": {"B": 720, "C": -720, "speed": 60}})});
         }
     }
 });
